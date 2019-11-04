c0615abf636e7b3d1f1ffb6fa43fa838b8868737
lmd <difedericolinda@gmail.com>
2019-11-03 23:46:10 -0500

    add save

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,24 @@
 var notes = require("../journal.json");
+var path = require("path");
 
 module.exports = function(app) {
   
   app.get("/api/notes", function(req, res) {
-    return res.json(notes);
+    res.sendFile(path.join(__dirname, "../db/db.json"));
+    // return res.json(notes);
   });
   
   app.post("/api/notes", function(req, res) {
     var newNote = req.body;
-    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+    // newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
     notes.push(newNote);
     res.json(newNote);
     console.log(newNote);
     // notes.push(req.body);
-    });
+  });
 
-  app.post("/api/notes/:id", function(req, res) {
-    var chosen = req.params.notes;
+  app.delete("/api/notes/:id", function(req, res) {
+    var chosen = res.params.notes;
     console.log(chosen);
 
     for (var i = 0; i < notes.length; i++) {
