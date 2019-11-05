37b9d020f316ffdfa0ac37fc3362bc10702a22ad
lmd <difedericolinda@gmail.com>
2019-11-04 19:54:11 -0500

    added save and delete functionality and update README

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 An application that can be used to write, save, and delete notes. Note Taker uses an express backend and saves and retrieves data from a JSON file.
 
+Note Taker is displayed across two pages.
+![Working Giphy](NoteTakerPages.gif)
+
+Note Taker can write notes.
+![Writing Giphy](NoteTakerWrite.gif)
+
+Note Taker can save notes.
+![Saving Giphy](NoteTakerSave.gif)
+
+Note Taker can delete notes.
+![Deleting Giphy](NoteTakerDelete.gif)
+
+**Credits**
+Giphy Capture 4.1
+
 **License**
 
 Licensed under the MIT License.
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,29 @@
-var notes = require("../journal.json");
+var notes = require("../db/db.json");
 var path = require("path");
+var fs = require("fs");
 
 module.exports = function(app) {
   
   app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../db/db.json"));
-    // return res.json(notes);
   });
   
   app.post("/api/notes", function(req, res) {
-    var newNote = req.body;
-    // newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
-    notes.push(newNote);
-    res.json(newNote);
-    console.log(newNote);
-    // notes.push(req.body);
+    fs.readFile(path.join(__dirname, "../db/db.json"), function() {
+      req.body.id = notes.length;
+      notes.push(req.body);
+      console.log(notes);
+      fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), function() {});
+    })
   });
 
   app.delete("/api/notes/:id", function(req, res) {
-    var chosen = res.params.notes;
-    console.log(chosen);
-
-    for (var i = 0; i < notes.length; i++) {
-      if (chosen === notes[i].routeName) {
-        return res.json(notes[i].length = 0);
-      }
-    }
-
-    // notes[i].length = 0; 
-    // res.json({ ok: true });
+    fs.readFile(path.join(__dirname, "../db/db.json"), function() {
+      var noteId = req.params.id;
+      var deleteNote = JSON.stringify(notes.filter(note => note.id != noteId))
+      fs.writeFile(path.join(__dirname, "../db/db.json"), deleteNote, () => {
+        console.log(`deleted`)
+      }); 
+    });
   });
 };
